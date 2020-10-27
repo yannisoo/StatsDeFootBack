563b03d44bd7a1346769ec8e9c139c0e9c710691
ybattist <yannis.battiston@capgemini.fr>
2020-10-27 15:50:08 +0100

    feat: :sparkles: cache implementation

--- server/api/routes/matchRoutes.js
+++ server/api/routes/matchRoutes.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = function(app) {
     let match = require('../controllers/matchController.js');
-
+    let cache = require('../cache/cache');
 
     app.route('/previsousMatchesBetween2teams/:team1/:team2')
         .get(match.previsousMatchesBetween2teams)
