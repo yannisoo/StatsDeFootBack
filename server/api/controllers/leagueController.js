7a4c72d68c562d116717c2cabf3c5b83757b1bfd
Pacski <remy.pachocinski@devinci.fr>
2020-11-01 23:47:53 +0100

    add country to league controller

--- server/api/controllers/leagueController.js
+++ server/api/controllers/leagueController.js
@@ -4,10 +4,12 @@ const API = require("../apiValue/apiValue.js")
 
 exports.getLeaguesByCountry = function(req, res) {
     let datatype = 'league';
+    let country = req.params.country
     let request = unirest("GET", API.url + 'v2/leagues/country/' + req.params.country + '/2020');
     request.headers(API.headers);
     request.then(function (APIresponse){
         APIresponse.body.api.datatype = datatype
+        APIresponse.body.api.country = country
         res.json(APIresponse.body.api);
     });
 }
