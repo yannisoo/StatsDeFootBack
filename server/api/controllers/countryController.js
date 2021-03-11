87b96c8297c0704006b4e1440b820e36b70740c4
yannisoo <yannis.b8@gmail.com>
2021-03-11 10:52:19 +0100

    changem API

--- server/api/controllers/countryController.js
+++ server/api/controllers/countryController.js
@@ -5,11 +5,12 @@ const API = require("../apiValue/apiValue.js");
 
 exports.getAll = function(req, res) {
     let datatype = 'country'
-    let request = unirest("GET", API.url + 'v2/countries');
+    let request = unirest("GET", API.url + 'countries');
     request.headers(API.headers);
     request.then(function (APIresponse){
-        APIresponse.body.api.datatype = datatype
-        res.json(APIresponse.body.api);
+        console.log(APIresponse.body)
+        APIresponse.body.datatype = datatype
+        res.json(APIresponse.body);
     })
         
 };
